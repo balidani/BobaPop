06510ce5b4b7473c5d41d1a598a4a25327eacf80
ilmikko <mikko@5x.fi>
2025-01-24 21:42:57 +0100

    Add bouncy bubbles

---
+++ src/bubble_path/bubble_path.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+
+var t = 0.0
+func _process(delta: float) -> void:
+	t += delta
+	if t < 1.0: return
+	
+	apply_central_impulse(-global_transform.basis.z * 2.0)
+	set_process(false)
