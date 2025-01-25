77850cbe82ae82d8871e5a698f8bc62c03fc544d
balidani <balijanosdaniel@gmail.com>
2025-01-25 18:17:29 +0100

    add new assets to segment generation

--- src/generated_level_segment/generated_level_segment.gd
+++ src/generated_level_segment/generated_level_segment.gd
@@ -42,7 +42,21 @@ func _ready():
 	if rng.randf() < 0.3:
 		# TODO: Rotate the special blocks.
 		var random_pos = Vector3i(rng.randi_range(-1, 1) * 2, 2, rng.randi_range(-1, 1) * 2)
-		_maybe_add_block(REPEATER.instantiate(), random_pos)
+		var random_kind = rng.randi_range(1, 3)
+		var instance = null
+		if random_kind == 0:
+			instance = REPEATER.instantiate()
+		elif random_kind == 1:
+			instance = NOTE_TONIC.instantiate()
+		else:
+			instance = NOTE_DOMINANT.instantiate()
+		
+		_maybe_add_block(instance, random_pos)
+	
+	# Make spikes
+	if rng.randf() < 0.1:
+		var random_pos = Vector3i(rng.randi_range(-1, 1) * 2, 2, rng.randi_range(-1, 1) * 2)
+		_maybe_add_block(SPIKE.instantiate(), random_pos)
 	
 	# Pepper in a few obstacles.
 	for _o in range(rng.randi_range(1, 3)):
@@ -50,8 +64,10 @@ func _ready():
 		_maybe_add_block(OBSTACLE.instantiate(), random_pos)
 
 
-# const SPIKE = preload(spike)
+const SPIKE = preload("res://src/spike/spike.tscn")
 const MASTER_POPPER = preload("res://src/tile_master_popper/tile_master_popper.tscn")
 const REPEATER = preload("res://src/repeater_obstacle/repeater_obstacle.tscn")
+const NOTE_TONIC = preload("res://src/note_tonic/note_tonic.tscn")
+const NOTE_DOMINANT = preload("res://src/note_dominant/note_dominant.tscn")
 const OBSTACLE = preload("res://src/tile_obstacle/tile_obstacle.tscn")
 const TILE_3W3L = preload("res://src/tile_3w3l/tile_3w3l.tscn")
