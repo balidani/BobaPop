4d0fe7babfc4f9d9ea7d36d9e89154a20b94c39e
ilmikko <mikko@5x.fi>
2025-01-26 00:54:27 +0100

    Obstacle tiles have different colours

--- src/glow_effect/glow_effect.gd
+++ src/glow_effect/glow_effect.gd
@@ -2,6 +2,12 @@ extends Node3D
 class_name GlowEffect
 
 
+var color :
+	set(c):
+		color = c
+		_light.light_color = c
+
+
 @onready var _ap : AnimationPlayer = $AnimationPlayer2
 @onready var _light : OmniLight3D = $OmniLight3D
 
