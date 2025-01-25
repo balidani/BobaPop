b34e0ee90e52841da31d8abf8e9610d1f574193a
ilmikko <mikko@5x.fi>
2025-01-25 13:35:37 +0100

    Fix tile obstacle animation

--- src/tile_obstacle/tile_obstacle.gd
+++ src/tile_obstacle/tile_obstacle.gd
@@ -7,5 +7,6 @@ class_name TileObstacle
 
 func bounce(_bubble: BouncyBubble, _last_velocity):
 	#_asp.play(0.0)
-	#_ap.play("bounce")
+	_ap.stop(false)
+	_ap.play("bounce")
 	Synth.player.play_note(440 * abs(_bubble.linear_velocity.x))
