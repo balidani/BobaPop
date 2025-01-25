c54b8cb5d34ee3517c2c1cd1678225622cd03145
balidani <balijanosdaniel@gmail.com>
2025-01-25 16:01:59 +0100

    add two note obstacles

---
+++ src/note_dominant/note_dominant.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+class_name NoteDominant
+
+@onready var _ap : AnimationPlayer = $AnimationPlayer
+@onready var _asp : AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+
+func bounce(_bubble: BouncyBubble, _last_velocity):
+	#_asp.play(0.0)
+	_ap.stop(false)
+	_ap.play("bounce")
+	Synth.player.play_note(659)
