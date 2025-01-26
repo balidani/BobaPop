e523f5b29a16cc8c57efe484c2971506216742ab
ilmikko <mikko@5x.fi>
2025-01-26 19:51:13 +0100

    Fix difficulty

--- src/difficulty/difficulty.gd
+++ src/difficulty/difficulty.gd
@@ -5,7 +5,7 @@ var difficulty = 0.0 :
 		difficulty = s
 		print("Difficulty set to %s" % s)
 
-var time_value = 0.0 :
+var time_value = 5.0 :
 	set(s):
 		time_value = s
 		print("Time value set to %s" % s)
