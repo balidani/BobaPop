cc515c6f0ccbab7d92074d51ed6c3f401fc99404
ilmikko <mikko@5x.fi>
2025-01-26 16:18:47 +0100

    Every commit I make takes me further away from the person I wish to be

--- src/tiles/tile_2w1l/tile_2w_1l.gd
+++ src/tiles/tile_2w1l/tile_2w_1l.gd
@@ -3,7 +3,9 @@ extends StaticBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Dirty hack to remove z fighting
+	var basis_amt = 0.0
+	var origin_amt = 0.1
 	var rng = RandomNumberGenerator.new()
 	rng.seed = str(global_transform).hash()
-	transform.origin += Vector3(rng.randf(), rng.randf(), rng.randf()).normalized() * 0.01
-	# transform.basis = Basis().rotated(transform.basis, 0.01)
+	transform.origin += Vector3(0, rng.randf(), 0) * origin_amt
+	transform.basis = Basis().rotated(transform.basis.x, basis_amt).rotated(transform.basis.z, basis_amt)
--- src/tiles/tile_3w3l/tile_3w_3l.gd
+++ src/tiles/tile_3w3l/tile_3w_3l.gd
@@ -1,17 +1,11 @@
 extends StaticBody3D
-class_name TileDefault
 
 
 func _ready() -> void:
 	# Dirty hack to remove z fighting
-	var basis_amt = 0.01
-	var origin_amt = 0.3
+	var basis_amt = 0.0
+	var origin_amt = 0.1
 	var rng = RandomNumberGenerator.new()
 	rng.seed = str(global_transform).hash()
 	transform.origin += Vector3(0, rng.randf(), 0) * origin_amt
 	transform.basis = Basis().rotated(transform.basis.x, basis_amt).rotated(transform.basis.z, basis_amt)
-
-
-
-func _on_area_3d_area_entered(_area: Area3D) -> void:
-	queue_free()
