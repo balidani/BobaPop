c3dd1065cc7d74c48cc3d0024c4eaabf24792faa
Dominik Maier <dmnk@google.com>
2025-01-26 01:26:57 +0100

    AMEN BROTHER

--- src/tiles/tile_amen/tile_amen.gd
+++ src/tiles/tile_amen/tile_amen.gd
@@ -1,19 +1,24 @@
 extends StaticBody3D
 class_name TileAmen
-
 # Plays AmenBros
 
 @onready var _ap : AnimationPlayer = $AnimationPlayer
+@onready var _asp_dot_net: AudioStreamPlayer3D = $AmenPlayer3d
 @onready var _glow : GlowEffect = $GlowEffect
 
+static var playing : bool = false
+
 func bounce(_bubble: BouncyBubble, _last_velocity):
-	_ap.play("bounce")
+	if playing:
+		_asp_dot_net.stop()
+		playing = false
+	else:
+		_asp_dot_net.play()
+		playing = true
 	
-	Audio.play("res://sounds/cw_amen01_175.wav")
-	
-	MusicRecorder.instance.play_note(
-		57, # TODO: More fun
-		"bounce",
+	MusicRecorder.instance.record(
+		NoteEvent.new(0, "amen", playing, not playing)
 	)
+
 	if LevelLighting.instance.dark_mode:
 		_glow.glow()
